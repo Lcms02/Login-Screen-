cbd1864ca82dce7edadc38a75db7f097ea9e3653
Lucas Meregali <lucas.smeregalli@gmail.com>
2022-09-25 13:08:15 -0300

    last comit

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,31 @@
-const inputs = document.querySelectorAll('.input')
-const button = document.querySelectorAll('.login-button')
+const inputs = document.querySelectorAll('.input');
+const button = document.querySelector('.login-button');
 
-const handleFocus = ({target}) =>{
-  const span = target.previousElementSibling; 
-  span.classList.add('span-active')
+const handleFocus = ({ target }) => {
+  const span = target.previousElementSibling;
+  span.classList.add('span-active');
 }
 
-const handleFocusOut = ({target}) =>{
-
-    if(target.value == ''){
-
-    const span = target.previousElementSibling; 
-    span.classList.remove('span-active')
-    }
+const handleFocusOut = ({ target }) => {
+  if (target.value === '') {
+    const span = target.previousElementSibling;
+    span.classList.remove('span-active');
   }
-
-const handleChange = () =>{
-    const [username, password] = inputs;
-
-    if(username.value && password.value.lenght >= 8){
-        button.removeAttribute('disabled');
-    } else{
-        button.setAttribute('disabled', '');
-    }
 }
 
-inputs.forEach((input) => input.addEventListener('focus', handleFocus));  
+const handleChange = () => {
+  const [username, password] = inputs;
 
-inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));  
+  if (username.value && password.value.length >= 8) {
+    button.removeAttribute('disabled');
+  } 
+  else {
+    button.setAttribute('disabled', '');
+  }
+}
+
+inputs.forEach((input) => input.addEventListener('focus', handleFocus));
+
+inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
 
 inputs.forEach((input) => input.addEventListener('input', handleChange));
